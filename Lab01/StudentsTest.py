777b58f7ab0c6462b456e142271cc162caf7e2fe
AlaRduTP <alardutp37259418@gmail.com>
2023-02-23 15:37:28 +0800

    Add testing log & Ignore if main statement

--- Lab01/StudentsTest.py
+++ Lab01/StudentsTest.py
@@ -10,13 +10,27 @@ class Test(unittest.TestCase):
 
     # test case function to check the Students.set_name function
     def test_0_set_name(self):
+        print('Start set_name test\n')
         for name in self.user_name:
             uid = self.students.set_name(name)
-            self.assertEqual(self.students.name[uid], name)
+            print(uid, name)
+            self.assertFalse(uid in self.user_id, uid)
+            self.user_id.append(uid)
+        print('\nFinish set_name test\n\n\n')
 
     # test case function to check the Students.get_name function
     def test_1_get_name(self):
-        for uid, name in enumerate(self.user_name):
+
+        print('\rStart get_name test\n')
+        print(f'user_id length =  {len(self.user_id)}')
+        print(f'user_name length =  {len(self.user_name)}\n')
+
+        for uid, name in zip(self.user_id, self.user_name):
             self.assertEqual(self.students.get_name(uid), name)
-        MEX = len(self.user_name)
-        self.assertEqual(self.students.get_name(MEX), 'There is no such user')
+            print(f'id {uid} : {name}')
+        mex = 0
+        while mex in self.user_id:
+            mex = mex + 1
+        self.assertEqual(self.students.get_name(mex), 'There is no such user')
+        print(f'id {uid} : There is no such user')
+        print('\nFinish get_name test', end='', flush=True)
